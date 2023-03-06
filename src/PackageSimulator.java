04d6ad9acbed966f86d44444bceb270c655ae574
hansun <hsun7920@bths.edu>
2023-03-05 23:53:36 -0500

    finished PostalCalculator

--- src/PackageSimulator.java
+++ src/PackageSimulator.java
@@ -1,14 +1,28 @@
 import java.util.ArrayList;
 
 public class PackageSimulator {
-
-    public String generateZip(){
+private final ArrayList<Package> generatedPs = new ArrayList<Package>();
+private final ArrayList<Double> costs = new ArrayList<Double>();
+    public Address generateAddress(){
         String zipThatISAString = "";
         int zip = (int) (Math.random() * 99442 + 501);
-        return zipThatISAString;
+        zipThatISAString = Integer.toString(zip);
+        Address a = new Address("000 ", "Dontexist Blvd ", "Whoville, ", "NY, ", zipThatISAString, ", Apt 999B");
+        return a;
+    }
+    public void generateTotal(){
+        for(int i = 0; i < generatedPs.size(); i++){
+
+        }
     }
 public void generatePackages (int num){
-    ArrayList<Package> generatedPs = new ArrayList<Package>();
-
+for(int i = 0; i < num; i++){
+    double lb = Math.random() * 149 + 1;
+    double w = Math.random() * 18 + 2;
+    double l = Math.random() * 18 + 2;
+    double h = Math.random() * 18 + 2;
+    Package p = new Package(generateAddress(), generateAddress(), l, h, w, lb);
+    generatedPs.add(p);
+}
 }
 }
